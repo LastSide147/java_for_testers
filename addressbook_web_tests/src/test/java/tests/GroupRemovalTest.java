9616dff05c3527020d2cf9d840e7cd41ee07ec7c
Артём <zemtsov100@gmail.com>
2025-09-30 20:23:52 +0300

    Exercise 6

---
+++ addressbook_web_tests/src/test/java/tests/GroupRemovalTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import model.GroupData;
+import org.junit.jupiter.api.Test;
+
+public class GroupRemovalTest extends TestBase {
+
+      @Test
+    public void CanRemoveGroup() {
+          app.groups().openGroupsPage();
+          if (!app.groups().isGroupPresent()) {
+            app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+        }
+          app.groups().removeGroup();
+      }
+}
