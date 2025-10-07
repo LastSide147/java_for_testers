78c7f71c4ec5659de751017c688a1411db04e62a
Артём <zemtsov100@gmail.com>
2025-10-07 22:19:11 +0300

    Lesson 4.3

--- addressbook_web_tests/src/test/java/tests/GroupRemovalTest.java
+++ addressbook_web_tests/src/test/java/tests/GroupRemovalTest.java
@@ -9,11 +9,20 @@ public class GroupRemovalTest extends TestBase {
       @Test
     public void CanRemoveGroup() {
           if (app.groups().getCount() == 0) {
-            app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+              app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
         }
           int groupCount = app.groups().getCount();
           app.groups().removeGroup();
           int newGroupCount = app.groups().getCount();
           Assertions.assertEquals(groupCount - 1, newGroupCount);
+      }
+
+      @Test
+    void canRemoveAllGroupsAtOnce() {
+          if (app.groups().getCount() == 0) {
+              app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+          }
+          app.groups().removeAllGroups();
+          Assertions.assertEquals(0, app.groups().getCount());
       }
 }
