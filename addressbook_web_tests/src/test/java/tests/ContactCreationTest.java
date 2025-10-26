db7358bd637f2b574229b9297f42ebc3697f034c
Артём <zemtsov100@gmail.com>
2025-10-26 13:05:09 +0300

    Exercise 10

--- addressbook_web_tests/src/test/java/tests/ContactCreationTest.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTest.java
@@ -14,30 +14,30 @@ public class ContactCreationTest extends TestBase {
     public static List<ContactData> contactProvider() {
         var result = new ArrayList<ContactData>();
         for (var firstname : List.of("", "name")) {
-//            for (var lastname : List.of("", "lastname")) {
-//                for (var address : List.of("", "address")) {
-//                    for (var phone : List.of("", "phone")) {
-//                        for (var email : List.of("", "email")) {
+            for (var lastname : List.of("", "lastname")) {
+                for (var address : List.of("", "address")) {
+                    for (var phone : List.of("", "phone")) {
+                        for (var email : List.of("", "email")) {
                             result.add(new ContactData()
-                                    .withFirstname(firstname));
-//                                    .withLastname(lastname)
-//                                    .withAddress(address)
-//                                    .withPhone(phone)
-//                                    .withEmail(email));
+                                    .withFirstname(firstname)
+                                    .withLastname(lastname)
+                                    .withAddress(address)
+                                    .withPhone(phone)
+                                    .withEmail(email));
                         }
-//                    }
-//                }
-//            }
-//        }
-//        for (int i = 0; i < 5; i++) {
-//            result.add(new ContactData()
-//                    .withFirstname(randomString(i * 10))
-//                    .withLastname(randomString(i * 10))
-//                    .withAddress(randomString(i * 10))
-//                    .withPhone(randomString(i * 10))
-//                    .withEmail(randomString(i * 10))
-//            );
-//        }
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            result.add(new ContactData()
+                    .withFirstname(randomString(i * 10))
+                    .withLastname(randomString(i * 10))
+                    .withAddress(randomString(i * 10))
+                    .withPhone(randomString(i * 10))
+                    .withEmail(randomString(i * 10))
+            );
+        }
         return result;
     }
 
