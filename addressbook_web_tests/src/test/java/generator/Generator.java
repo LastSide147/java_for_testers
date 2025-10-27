1cae8a5196d2e190ba3d8f607dd8437342d534bf
Артём <zemtsov100@gmail.com>
2025-10-27 21:18:35 +0300

    5.3 complete

--- addressbook_web_tests/src/test/java/generator/Generator.java
+++ addressbook_web_tests/src/test/java/generator/Generator.java
@@ -1,18 +1,32 @@
 package generator;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.JCommander;
+import common.CommonFunctions;
+import model.GroupData;
+
+import java.util.ArrayList;
 
 public class Generator {
 
     @Parameter(names={"--type", "-t"})
     String type;
 
-    @Parameter(names={"--type", "-t"})
-    String type;
+    @Parameter(names={"--output", "-o"})
+    String output;
 
+    @Parameter(names={"--format", "-f"})
+    String format;
+
+    @Parameter(names={"--count", "-n"})
+    int count;
 
     public static void main(String[] args) {
-        new Generator().run();
+        var generator = new Generator();
+        JCommander.newBuilder()
+                .addObject(generator)
+                .build()
+                .parse(args);
+        generator.run();
     }
 
     private void run() {
@@ -21,10 +35,32 @@ public class Generator {
     }
 
     private Object generate() {
+        if ("groups".equals(type)) {
+            return generateGroups();
+        }
+        else if ("contacts".equals(type)) {
+            return generateContacts();
+        } else {
+            throw new IllegalArgumentException("Unknown datatype" + type);
+        }
+    }
+
+    private Object generateGroups() {
+        var result = new ArrayList<GroupData>();
+        for (int i = 0; i < count; i++) {
+            result.add(new GroupData()
+                    .withName(CommonFunctions.randomString(i * 10))
+                    .withHeader(CommonFunctions.randomString(i * 10))
+                    .withFooter(CommonFunctions.randomString(i * 10)));
+        }
+        return result;
+    }
+
+    private Object generateContacts() {
         return null;
     }
 
-    private void save(Object data) {
 
+    private void save(Object data) {
     }
 }
