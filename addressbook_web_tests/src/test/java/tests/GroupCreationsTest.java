e72e830fcd20a56132d391a7a78b8b34d2339bb2
Артём <zemtsov100@gmail.com>
2025-10-28 21:47:55 +0300

    Exercise 13

--- addressbook_web_tests/src/test/java/tests/GroupCreationsTest.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationsTest.java
@@ -32,7 +32,7 @@ public class GroupCreationsTest extends TestBase {
 //        }
 
         var json = "";
-        try (var reader = new FileReader("groups.json");
+        try (var reader = new FileReader("groupString.json");
              var breader = new BufferedReader(reader)
         ) {
             var line = breader.readLine();
