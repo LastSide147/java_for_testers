21f6ef526c33f439e408214bc5b33bb2a455d3c2
Артём <zemtsov100@gmail.com>
2025-10-31 20:06:46 +0300

    6.1 OldGroups = 0

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -18,6 +18,7 @@ public class ApplicationManager {
 
     private GroupHelper groups;
     private ContactHelper contacts;
+    private JdbcHelper jdbc;
 
     private Properties properties;
 
@@ -58,6 +59,13 @@ public class ApplicationManager {
             contacts = new ContactHelper(this);
         }
         return contacts;
+    }
+
+    public JdbcHelper jdbc() {
+        if( jdbc == null) {
+            jdbc = new JdbcHelper(this);
+        }
+        return jdbc;
     }
 
     protected boolean isElementPresent(By locator) {
---
+++ addressbook_web_tests/src/test/java/manager/JdbcHelper.java
@@ -0,0 +1,32 @@
+package manager;
+
+import model.GroupData;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JdbcHelper extends HelperBase{
+    public JdbcHelper(ApplicationManager manager) {super (manager); }
+
+    public List<GroupData> getCountGroupList() {
+        var groups = new ArrayList<GroupData>();
+        try (var conn = DriverManager.getConnection("jdbc:mysql://localhost/addressbook", "root", "");
+            var statement = conn.createStatement();
+             var result = statement.executeQuery("select group_id, group_name, group_header, group_footer from group_list"))
+        {
+            while (result.next()) {
+                groups.add(new GroupData()
+                        .withId(result.getString("groiup_id"))
+                        .withName(result.getString("group_name"))
+                        .withHeader(result.getString("group_header"))
+                        .withFooter(result.getString("group_footer")));
+            }
+        } catch (SQLException e) {
+
+        }
+        return groups;
+    }
+}
