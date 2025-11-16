22ddd5e8ed320d3f49963942693385a5bbd39594
Артём <zemtsov100@gmail.com>
2025-11-16 21:22:03 +0300

    8.4

--- mantis_test/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis_test/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -1,6 +1,5 @@
 package ru.stqa.mantis.manager;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,6 +12,8 @@ public class ApplicationManager {
     private String string;
     private Properties properties;
     private SessionHelper sessionHelper;
+    private HttpSessionHelper httpSessionHelper;
+    private JamesCliHelper jamesCliHelper;
 
     public void init(String browser, Properties properties) {
         this.string = browser;
@@ -43,4 +44,21 @@ public class ApplicationManager {
         return sessionHelper;
     }
 
+    public HttpSessionHelper http() {
+        if(httpSessionHelper == null) {
+            httpSessionHelper = new HttpSessionHelper(this);
+        }
+        return httpSessionHelper;
+    }
+
+    public JamesCliHelper jamesCli() {
+        if(jamesCliHelper == null) {
+            jamesCliHelper = new JamesCliHelper(this);
+        }
+        return jamesCliHelper;
+    }
+
+    public String property(String name) {
+        return properties.getProperty(name);
+    }
 }
---
+++ mantis_test/src/test/java/ru/stqa/mantis/manager/JamesCliHelper.java
@@ -0,0 +1,39 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.os.ExternalProcess;
+import ru.stqa.mantis.common.CommonFunctions;
+
+import java.time.Duration;
+
+public class JamesCliHelper extends HelperBase{
+
+     public JamesCliHelper(ApplicationManager manager) {
+         super(manager);
+     }
+
+     public void addUser(String email, String password) throws InterruptedException {
+         ExternalProcess.builder()
+                 .command("java",
+                         "-cp",
+                         "\"james-server-jpa-app.lib/*\"",
+                         "org.apache.james.cli.ServerCmd",
+                         "--host", "127.0.0.1",
+                         "--port", "9999",
+                         "AddUser", email, password)
+//                 .directory("D:/james-server-jpa-guice/james-server-jpa-guice")
+                 .directory(manager.property("james.workingDir"))
+                 .copyOutputTo(System.err)
+                 .start()
+                 .waitFor(Duration.ofHours(1));
+
+
+//         ProcessBuilder cmd = new ProcessBuilder(
+//                 "java",
+//                 "-cp",
+//                 "\"james-server-jpa-app.lib/*\"",
+//                 "org.apache.james.cli.ServerCmd",
+//                 "AddUser", email, password);
+//         cmd.execute();
+//         cmd.waitFor();
+     }
+}
